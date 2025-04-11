ba14f0d33ec28039878fd8a2c86485c2f84d8d61
Pedro Franceschin <103785545+pbfranceschin@users.noreply.github.com>
2025-04-11 14:48:39 +0000

    feat: inverted loading and added help button

--- chessGame_react/src/components/UI/Loading/Loading.js
+++ chessGame_react/src/components/UI/Loading/Loading.js
@@ -15,26 +15,50 @@ export const Loading = ({ title, onClickRefund, roomName }) => {
   useEffect(() => {
     timeInterval.current = setInterval(() => {
       setCurrentTime((prev) => prev + 1);
-      if (currentTime > 10) {
-        if(roomName != "Classic Room") setRefund(true);
+      if (currentTime > 15) {
+        if (roomName != "Classic Room") setRefund(true);
         setCurrentTime(0);
       }
     }, 1000);
 
     return () => clearInterval(timeInterval.current);
-  });
+  }, []);
+
+  const getStarStatus = (idx) => {
+    const phase = currentTime % 7;
+
+    if (phase === 0) return false;
+
+    switch (phase) {
+      case 1:
+        return idx === 5;
+      case 2:
+        return idx >= 4;
+      case 3:
+        return idx >= 3;
+      case 4:
+        return idx >= 2;
+      case 5:
+        return idx >= 1;
+      case 6:
+        return idx >= 0;
+      default:
+        return false;
+    }
+  };
 
   return (
     <div className="loading">
+      <button style={{ position: "fixed", top: "1rem", right: "1rem" }}>
+        Help
+      </button>
       <div className="loading__container">
         <div className="loading__stars">
           {starArray.map((item, idx) => (
             <img
               key={`star_${idx}`}
               className="star"
-              src={
-                idx <= currentTime % starArray.length ? star_full : star_none
-              }
+              src={getStarStatus(idx) ? star_full : star_none}
               alt="pic"
             />
           ))}
